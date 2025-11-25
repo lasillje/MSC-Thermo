620733f992636fc79cfeb7d4a632ea0b4bcf9943
Laurens Sillje <laurenssillje+dev@gmail.com>
2025-11-25 14:59:47 +0100

    Added ThermoModel I/O functions to export/load to Cobra base models for use in PTA. pta.prepare_for_pta is functional now.

---
+++ scripts/io.py
@@ -0,0 +1,105 @@
+import cobra.io
+import json
+import numpy as np
+from io import StringIO
+from cobra import Model, Reaction, Metabolite
+
+def _sanitize_value(value, replacement=0.0):
+    if isinstance(value, float) and (np.isnan(value) or np.isinf(value)):
+        return replacement
+    return value
+
+def export_tmodel_cobra(tmodel, output_path):
+    """
+    Exports a ThermoModel to a JSON file to load in later as a Cobra model
+    """
+
+    data = {
+        "id": tmodel.id,
+        "name": tmodel.name,
+        "reactions": [],
+        "metabolites": [],
+        "genes": [g.id for g in tmodel.genes],
+        "notes": tmodel.notes,
+        "annotation": tmodel.annotation
+    }
+
+    for met in tmodel.metabolites:
+        met_data = {
+            "id": met.id,
+            "name": met.name,
+            "compartment": met.compartment,
+            "charge": _sanitize_value(met.charge),
+            "formula": met.formula,
+        }
+        data["metabolites"].append(met_data)
+
+
+    met_map = {met.id: met for met in tmodel.metabolites}
+
+    for rxn in tmodel.reactions:
+        lb_clean = _sanitize_value(rxn.lower_bound, replacement=-1000.0)
+        ub_clean = _sanitize_value(rxn.upper_bound, replacement=1000.0)
+        
+        stoichiometry = {}
+        for met_id, coeff in rxn.metabolites.items():
+             stoichiometry[met_id.id] = _sanitize_value(coeff, replacement=0.0)
+
+        rxn_data = {
+            "id": rxn.id,
+            "name": rxn.name,
+            "lower_bound": lb_clean,
+            "upper_bound": ub_clean,
+            "gene_reaction_rule": rxn.gene_reaction_rule,
+            "metabolites": stoichiometry,
+            "subsystem": rxn.subsystem,
+        }
+        data["reactions"].append(rxn_data)
+
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=4)
+        
+    print(f"Successfully exported clean COBRA data to {output_path}")
+    return output_path
+
+
+def load_tmodel_cobra(input_path):
+    """
+    Loads JSON file to create a cobra model
+    """
+
+    with open(input_path, 'r') as f:
+        data = json.load(f)
+
+    new_model = Model(data["id"])
+    new_model.name = data["name"]
+    new_model.notes = data.get("notes", {})
+    new_model.annotation = data.get("annotation", {})
+
+    metabolite_objects = {}
+    for met_data in data["metabolites"]:
+        met = Metabolite(**met_data)
+        metabolite_objects[met.id] = met
+
+    new_model.add_metabolites(list(metabolite_objects.values()))
+
+    reaction_objects = []
+    for rxn_data in data["reactions"]:
+        rxn = Reaction(rxn_data["id"])
+        rxn.name = rxn_data["name"]
+        rxn.lower_bound = rxn_data["lower_bound"]
+        rxn.upper_bound = rxn_data["upper_bound"]
+        rxn.gene_reaction_rule = rxn_data["gene_reaction_rule"]
+        rxn.subsystem = rxn_data["subsystem"]
+
+        stoichiometry = {}
+        for met_id, coeff in rxn_data["metabolites"].items():
+            stoichiometry[metabolite_objects[met_id]] = coeff
+        
+        rxn.add_metabolites(stoichiometry)
+        reaction_objects.append(rxn)
+
+    new_model.add_reactions(reaction_objects)
+
+    print(f"Successfully loaded clean COBRA model: {new_model.id}")
+    return new_model
