f368e0ea81e9af4c9cd330ca21640984e72dd475
Laurens Sillje <laurenssillje+dev@gmail.com>
2026-02-10 02:06:37 +0100

    Cleanup

--- regression/README.md
+++
@@ -1,2 +0,0 @@
-The files contained in this folder where taken from 
-`https://github.com/tednsmith/thermo_flux/tree/main/examples/ecoli/GSM`
--- scripts/logger.py
+++
@@ -1,5 +0,0 @@
-def write_to_log(filename, message: str, mode="a", skip=True):
-    if not skip:
-        if filename is not None or filename != "" or filename:
-            with open(filename, mode) as out:
-                out.write(f"{message}\n")
